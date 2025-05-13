bb8565263bca4ee4c1e65dbe3baea236706d691f
aya <aw2744@yahoo.co.jp>
2025-05-13 20:35:27 +0900

    🗃️ データベース設定をPostgreSQLに変更し、docker-compose.ymlを追加。

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = (False,)
-    SQLALCHEMY_DATABASE_URI = "sqlite:///Timerdb.sqlite"
+    SQLALCHEMY_DATABASE_URI = "postgresql://user:password@localhost:5432/Timer"
