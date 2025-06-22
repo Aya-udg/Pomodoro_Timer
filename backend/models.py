bd6c25379a82e37681a62aff52107ca2a4071395
aya <aw2744@yahoo.co.jp>
2025-06-22 10:42:50 +0900

    バックエンドをfastapiに変更

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,1 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class Timer(db.Model):
-    __tablename__ = "timer"
-    id = db.Column(db.Integer, primary_key=True)
-    set_time = db.Column(db.DateTime, nullable=False)
-    break_time = db.Column(db.DateTime, nullable=False)
-    long_break_time = db.Column(db.DateTime, nullable=False)
+from sqlmodel import Field, SQLModel
