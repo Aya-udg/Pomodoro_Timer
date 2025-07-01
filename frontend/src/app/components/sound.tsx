1b68c091f17ac7b2fd3e19335868fcb2985acce4
aya <aw2744@yahoo.co.jp>
2025-07-01 19:32:06 +0900

    アラーム音を追加

--- frontend/src/app/components/sound.tsx
+++ frontend/src/app/components/sound.tsx
@@ -1,12 +1,12 @@
 import useSound from "use-sound";
 
 export const BoopButton = () => {
-  const [play, { stop }] = useSound("/sounds/alarm.mp3");
+  // const [play, { stop }] = useSound("/sounds/fanfare.mp3");
+  const [play, { stop }] = useSound("/sounds/cuckoo_clock.mp3");
 
   return (
     <>
-      <button onClick={() => play()}> Boop! </button>
-      <button onClick={() => stop()}>停止</button>
+      <button onClick={() => play()}> 作業終了 </button>
     </>
   );
 };
