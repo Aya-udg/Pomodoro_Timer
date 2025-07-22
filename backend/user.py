baa1629cf114fa9387321a7ef2f64b14c038b0c2
aya <aw2744@yahoo.co.jp>
2025-07-23 07:36:45 +0900

    現在のユーザー情報を取得するエンドポイントを修正

--- backend/user.py
+++ backend/user.py
@@ -113,13 +113,26 @@ async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
 ) -> Token:
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user 
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
-@app.get("/items")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
 
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
+# 現在のユーザー情報を取得するエンドポイント
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
+
+# 現在のユーザーのアイテムを取得するエンドポイント
+@app.get("/users/me/items")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
