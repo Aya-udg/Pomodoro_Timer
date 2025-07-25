04ba4053a71aa318364d9f736cad837c489aa688
aya <aw2744@yahoo.co.jp>
2025-07-26 08:06:15 +0900

    ログインフォームの作成

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,10 +1,65 @@
- import type { NextPage } from "next";
- 
- const Page: NextPage = () => {
-   return (
-     <>
-     </>
-   )
- }
- 
- export default Page
+import { Button } from "@/app/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { Input } from "@/app/components/ui/input";
+import { Label } from "@/app/components/ui/label";
+
+export default function app() {
+  return (
+    <div className="w-full flex items-center justify-cente max-w-sm">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>ログイン</CardTitle>
+          <CardDescription>
+            ログインするにはパスワードとメールアドレスを入力してください
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">サインアップ</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">メールアドレス</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    パスワードをお忘れの方
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            ログイン
+          </Button>
+          <Button variant="outline" className="w-full">
+            新規会員登録
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
