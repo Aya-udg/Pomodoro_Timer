2f6a4d6b871d504fbb34a49cbd92eaacab1a9b0a
aya <aw2744@yahoo.co.jp>
2025-08-05 20:57:22 +0900

    フロントエンドのdockerfileの作成

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from models.models import Timer, StudyHistory
 from api import study_record, users
 
 app = FastAPI()
