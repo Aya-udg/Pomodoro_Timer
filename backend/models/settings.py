ef32b7c2389b0ae0eed5ccd99e8f58ebe9542bb0
aya <aw2744@yahoo.co.jp>
2025-08-17 16:10:52 +0900

    バックエンドのリファクタリング

--- backend/models/settings.py
+++ backend/models/settings.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 from sqlmodel import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv(dotenv_path="../../.env")
+# load_dotenv(dotenv_path="../../.env")
+# path = os.getenv("POSTGRES_URL")
+
+load_dotenv()
 path = os.getenv("POSTGRES_URL")
+
 
 engine = create_engine(path, echo=True)
 Base = declarative_base()
