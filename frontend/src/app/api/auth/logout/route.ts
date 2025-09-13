6fd95e2e5d61cb3d60190d4be663e763110dbe46
aya <aw2744@yahoo.co.jp>
2025-09-13 09:54:30 +0900

    ログアウトエンドポイントの修正、Dockerfileのコピー命令の整理、API呼び出しのエラーハンドリング改善、Nginx設定の修正

--- frontend/src/app/api/auth/logout/route.ts
+++ frontend/src/app/api/auth/logout/route.ts
@@ -6,25 +6,22 @@ const DB_URL = process.env.NEXT_PUBLIC_API_URL;
 export async function POST() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
-
   if (!token)
     return NextResponse.json(
       { error: "ログインしていません" },
       { status: 400 }
     );
+  const res = await fetch(`${DB_URL}/logout`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) return NextResponse.json({ error: "エラー" }, { status: 500 });
   else {
-    const res = await fetch(`${DB_URL}/logout`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!res.ok) return NextResponse.json({ error: "エラー" }, { status: 500 });
-    else {
-      const response = NextResponse.json({ message: "ログアウトしました" });
-      response.cookies.delete("token");
-      response.cookies.delete("refresh_token");
-      return response;
-    }
+    const response = NextResponse.json({ message: "ログアウトしました" });
+    response.cookies.delete("token");
+    response.cookies.delete("refresh_token");
+    return response;
   }
 }
